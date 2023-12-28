16b514fbfd928fb97abd41ffe2dabdfcb22f97d0
Deep <dhamaladeep2@gmail.com>
2023-12-28 20:10:27 +0545

    first hit at median q no 4

---
+++ 3 Longest Substring/Median.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Median {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int n = nums1.length+nums2.length;
+        int median = (n+1)/2;
+        return median;
+
+    }
+}
