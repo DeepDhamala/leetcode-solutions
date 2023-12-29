c9a050b80d06edc2663bc5163457f0cbfa135014
Deep <dhamaladeep2@gmail.com>
2023-12-29 22:17:53 +0545

    removed unwanted files on .gitignore

--- out/production/leetcode-solutions/TwoSum.py
+++
@@ -1,9 +0,0 @@
-class TwoSum:
-    def two_sum(self, nums, target):
-        num_set = set()
-        for i, num in enumerate(nums):
-            complementary = target - num
-            if complementary in num_set:
-                return [nums.index(complementary),i]
-            num_set.add(num)
-        return None
