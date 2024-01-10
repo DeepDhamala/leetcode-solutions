855f0dabbe55c4aa0f6d3cb025daa185d605da5e
Deep <dhamaladeep2@gmail.com>
2024-01-10 09:51:15 +0545

    palindromic substring brutforce method

--- 5 Longest Palindromic Substring/PalindromicSubstring.java
+++ 5 Longest Palindromic Substring/PalindromicSubstring.java
@@ -2,16 +2,35 @@ import java.util.HashMap;
 import java.util.Map;
 
 class Solution {
-    public String longestPalindrome(String s) {
-        StringBuffer sb = new StringBuffer();
-        Map<Integer, String> map = new HashMap<>();
-        for(int i = 0; i <s.length(); i++){
-            for(int j= 0; j<i; i++){
-                if(map.containsValue(s.charAt(j))){
-                    
-                }
-            }
-        } 
-        return null;
+   //bruteforce method
+   public String longestPalindrome(String s){
+    if(s.length() <=1 ){
+        return s;
     }
+    int maxLen = 1;
+    String maxStr = s.substring(0, 1);
+    for (int i = 0; i<s.length(); i++){
+        for(int j= i+maxLen; j<s.length(); j++){
+            if(j-i > maxLen && isPalindrome(s.substring(i,j))){
+                maxLen = j-i;
+                maxStr=s.substring(i,j);
+            }
+        }
+    }
+    return maxStr;
+   }
+   private boolean isPalindrome(String str){
+    int left = 0;
+    int right = str.length() - 1;
+
+    while (left < right){
+        if(str.charAt(left) != str.charAt(right)){
+            return false;
+        }
+        left ++;
+        right --;
+
+    }
+    return true;
+   }
 }
