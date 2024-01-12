852dbb932139c787f2f6466c13aabaeee822e00b
Deep <dhamaladeep2@gmail.com>
2024-01-12 22:52:57 +0545

    zizgag first play

---
+++ 5 Longest Palindromic Substring/ZigZag.java
@@ -0,0 +1,28 @@
+public class ZigZag {
+    public String convert(String s, int numRows){
+        //Define StringBuilders
+        StringBuilder[] sbs = new StringBuilder[numRows];
+        for(int i = 0; i<numRows; i++){
+            sbs[i] = new StringBuilder();
+        }
+    }
+    //Define Variables
+    char[] arr = s.toCharArray();
+    int n = arr.length;
+    int index =0;
+
+    //Tranverse zig zig
+    while(index <n){
+        //GO down
+        for(int j=0; j<numRows && index < n; j++){
+            sbs[j].append(arr[index++]);
+        }
+
+    }
+    //combine all stringbuiders into one
+    StringBuilder res = sbs[0];
+    for(int i = 1; i<numRows; i++){
+        res.append(sbs[i].toString());
+    }return res.toString();
+}
+}
