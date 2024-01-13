daef20fefee08312098219ccdbe450d5136265a1
Deep <dhamaladeep2@gmail.com>
2024-01-13 21:12:03 +0545

    zigzag conversion perpected

--- ZigZag COnversion/ZigZag.java
+++ ZigZag COnversion/ZigZag.java
@@ -5,16 +5,17 @@ public class ZigZag {
         for(int i = 0; i<numRows; i++){
             sbs[i] = new StringBuilder();
         }
-    }
-    //Define Variables
-    char[] arr = s.toCharArray();
-    int n = arr.length;
-    int index =0;
-
-    //Tranverse zig zig
+        //Define Variables
+        char[] arr = s.toCharArray();
+        int n = arr.length;
+        int index =0;
+        //Tranverse zig zig
     while(index <n){
         //GO down
         for(int j=0; j<numRows && index < n; j++){
+            sbs[j].append(arr[index++]);
+        }
+        for (int j=numRows -2; j>0 && index <n; j--){
             sbs[j].append(arr[index++]);
         }
 
@@ -23,6 +24,13 @@ public class ZigZag {
     StringBuilder res = sbs[0];
     for(int i = 1; i<numRows; i++){
         res.append(sbs[i].toString());
-    }return res.toString();
+    }
+    return res.toString();
 }
 }
+
+    
+    
+
+    
+    
