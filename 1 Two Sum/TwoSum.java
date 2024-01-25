cf8d41f36243b295663a4583faa59e5e18d1e8a8
Deep Dhamala <dhamaladeep2@gmail.com>
2024-01-25 13:05:51 +0545

    revsion of two sum

--- 1 Two Sum/TwoSum.java
+++ 1 Two Sum/TwoSum.java
@@ -2,15 +2,16 @@ import java.util.HashMap;
 import java.util.Map;
 
 class TwoSum {
-    public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for(int i=0; i<nums.length; i++){
-            int complementary = target - nums[i];
-            if(map.containsKey(complementary)){
-                return new int[] {map.get(complementary),i};
-            }
-            map.put(nums[i], i);
-        }
-        return null;
-    }
+   public int[] twoSum (int[] nums, int target){
+       Map<Integer, Integer> map = new HashMap<>();
+       for(int i = 0; i< nums.length; i++){
+           int complement = target - nums[i];
+           if(map.containsKey(complement)){
+               return new int[] {i, map.get(complement)};
+           }
+           map.put(nums[i], i);
+
+       }
+       return new int[] {-1};
+   }
 }
