3bc1dbae5545abe0d7c305264e5e55b21344574c
Deep Dhamala <dhamaladeep2@gmail.com>
2024-01-26 09:53:08 +0545

    add two numbers re

--- 2 Add Two Numbers/AddTwoNumbers.java
+++ 2 Add Two Numbers/AddTwoNumbers.java
@@ -1,39 +1,33 @@
 
-class ListNode {
-     int val;
-     ListNode next;
-     ListNode() {}
-     ListNode(int val) { this.val = val; }
-     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- }
+
+  class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+   ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
 
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode result = new ListNode(0);
-        ListNode tracker = result;
-        int carry =0;
-        while(l1 != null || l2 != null){
-            int x = l1 != null ? l1.val : 0;
+        ListNode pointer = result;
+        int carry = 0;
+        while(l1 != null || l2!=null){
+            int x = l1 !=null ? l1.val : 0;
             int y = l2 !=null ? l2.val : 0;
             int sum = x+y+carry;
             int digit = sum%10;
-            carry = sum / 10;
-            tracker.next = new ListNode(digit);
-            tracker = tracker.next;
-            if(l1!=null){
-                l1=l1.next;
-            }
-            if(l2!=null){
-                l2=l2.next;
-            }
+            carry = sum/10;
+            pointer.next = new ListNode(digit);
+            pointer = pointer.next;
+            if (l1 != null) l1 = l1.next;
+            if(l2 != null) l2=l2.next;
+
         }
-        if (carry >0){
-            tracker.next = new ListNode(carry);
+        if (carry>0){
+            pointer.next = new ListNode(carry);
         }
         return result.next;
-    }
-
-    public int[] merge(int[] is, int[] is2) {
-        return null;
     }
 }
