444a42077e2f00f682a9a39b7b236d01256da39f
Deep Dhamala <dhamaladeep2@gmail.com>
2024-01-26 21:39:12 +0545

    longest substring re

--- 3 Longest Substring/LongestSubstring.java
+++ 3 Longest Substring/LongestSubstring.java
@@ -1,13 +1,13 @@
 public class LongestSubstring {
-    public int lengthofLongestSubstring(String s){
-    int maxLength = 0;
-    for (int left = 0, right = 0; right<s.length(); right++){
-        int foundIndex = s.indexOf(s.charAt(right), left);
-        if(foundIndex != right){
-            left = foundIndex + 1;
+    public int lengthOfLongestSubstring(String s){
+        int maxLength = 0;
+        for (int left = 0, right = 0; right < s.length(); right++){
+            int foundIndex = s.indexOf(s.charAt(right), left);
+            if(foundIndex != right){
+                left = foundIndex +1;
+            }
+            maxLength = Math.max(maxLength, right - left +1);
         }
-        maxLength = Math.max(maxLength, right - left + 1);
+        return maxLength;
     }
-    return maxLength;
-}
 }
